4944a3be63a7f1f7ebf3281fe85b0f101fb4747f
syifaandini <syifandin@gmail.com>
2020-02-11 08:30:54 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+  // function playGif(){
+  $("document").ready(function(){
+    $("#artha-project1-img, #artha-project1-text").hover(
+        function() {
+            $("#artha-project1-img").attr("src", "images/project/gedungsate.gif");
+        },
+        function() {
+            $("#artha-project1-img").attr("src", "images/project/gunung.jpg");
+        }
+    );
+
+    $("#artha-project2-img, #artha-project2-text").hover(
+        function() {
+            $("#artha-project2-img").attr("src", "images/project/merbabu.gif");
+        },
+        function() {
+            $("#artha-project2-img").attr("src", "images/project/langit.jpg");
+        }
+    );
+
+    $("#artha-project3-img, #artha-project3-text").hover(
+        function() {
+            $("#artha-project3-img").attr("src", "images/project/merbabu2.gif");
+        },
+        function() {
+            $("#artha-project3-img").attr("src", "images/project/gunung2.jpg");
+        }
+    );
+  });
+
+  // }
+
+  // window.onload = function(){
+  //   playGif();
+  // }
