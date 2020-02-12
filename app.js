03d115c68c3bda23295b9ac09345a3d34374f5f3
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-02-12 18:47:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ $( document ).ready(function() {
 
   var plane = document.getElementById('logoku');
   plane.homePos = { x: plane.offsetLeft, y: plane.offsetTop };
-  $('#wrapper_header').mousemove(function (e) {
+  $('#title').mousemove(function (e) {
         parallax(e, plane, 100);
     });
 });
