126eccd0e251fbe758d066a13a953e4314f4130a
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-02-12 20:38:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -44,49 +44,18 @@ $( document ).ready(function() {
   );
 
 
-  $('#title').mousemove(
-        function(e){
-            /* Detecta a posição do mouse */
-            var offset = $(this).offset();
-            var xPos = e.pageX - offset.left;
-            var yPos = e.pageY - offset.top;
-
-            var mouseXPercent = Math.round(xPos / $(this).width() * 100);
-            var mouseYPercent = Math.round(yPos / $(this).height() * 100);
-            $(this).children('img').each(
-                function(){
-                    var diffX = $('#title').width() - $(this).width();
-                    var diffY = $('#title').height() - $(this).height();
-
-                    var myX = diffX * (mouseXPercent / 100); //) / 100) / 2;
-
-                    var myY = diffY * (mouseYPercent / 100);
-
-                    var cssObj = {
-                        'left': myX + 'px',
-                        'top': myY + 'px'
-                    }
-                    //$(this).css(cssObj);
-                    $(this).animate({
-                        left: myX, 
-                        top: myY
-                    },
-                    {
-                        duration: 50, 
-                        queue: false, 
-                        easing: 'linear'
-                    });
-
-                }
-            );
-});
+  $('#title').mousemove(function (e) {
+        //parallax(e, this, 1);
+        parallax(e, document.getElementById('logoku'), 0.25);
+    });
 
  
 });
 
 
 function parallax(e, target, layer) {
-    var x = target.homePos.x - (e.pageX - target.homePos.x) / layer;
-    var y = target.homePos.y - (e.pageY - target.homePos.y) / layer;
+    var layer_coeff = 10 / layer;
+    var x = ($(window).width() - target.offsetWidth-40) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
+    var y = ($(window).height() - target.offsetHeight-155) / 2 - (e.pageY - ($(window).height() / 2)) / layer_coeff;
     $(target).offset({ top: y ,left : x });
 };
