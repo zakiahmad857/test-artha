9bc3761c134f5d8f1eb2b0155ca68ad5d382b9bd
syifaandini <syifandin@gmail.com>
2020-02-12 14:33:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-
+// NAVBAR SCROLL HIDE-SHOW
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    $("#navbarArtha").css({top: '0'});
+    $("#navbarArtha").css({
+      top: '0'
+    });
   } else {
-    $("#navbarArtha").css({top: '-60px'});
+    $("#navbarArtha").css({
+      top: '-60px'
+    });
   }
   prevScrollpos = currentScrollPos;
 }
 
-// function playGif(){
-  $(document).ready(function(){
-    //alert("hai");
-    $("#artha-project1-all").hover(
-      function() {
-        $("#artha-project1-img").attr("src", "images/project/gedungsate.gif");
-      },
-      function() {
-        $("#artha-project1-img").attr("src", "images/project/gunung.jpg");
-      }
-    );
+// PROJECT - GIFs
+$(document).ready(function() {
+  $("#artha-project1-all").hover(
+    function() {
+      $("#artha-project1-img").attr("src", "images/project/gedungsate.gif");
+    },
+    function() {
+      $("#artha-project1-img").attr("src", "images/project/gunung.jpg");
+    }
+  );
 
-    $("#artha-project2-all").hover(
-      function() {
-        $("#artha-project2-img").attr("src", "images/project/merbabu.gif");
-      },
-      function() {
-        $("#artha-project2-img").attr("src", "images/project/langit.jpg");
-      }
-    );
+  $("#artha-project2-all").hover(
+    function() {
+      $("#artha-project2-img").attr("src", "images/project/merbabu.gif");
+    },
+    function() {
+      $("#artha-project2-img").attr("src", "images/project/langit.jpg");
+    }
+  );
 
-    $("#artha-project3-all").hover(
-      function() {
-        $("#artha-project3-img").attr("src", "images/project/merbabu2.gif");
-      },
-      function() {
-        $("#artha-project3-img").attr("src", "images/project/gunung2.jpg");
-      }
-    );
-    });
-
-    // }
-
-    // window.onload = function(){
-      //   playGif();
-      // }
-
-// $("#artha-project1-img").
+  $("#artha-project3-all").hover(
+    function() {
+      $("#artha-project3-img").attr("src", "images/project/merbabu2.gif");
+    },
+    function() {
+      $("#artha-project3-img").attr("src", "images/project/gunung2.jpg");
+    }
+  );
+});
