f0ee9ff0ecfb59db45e47e35020ec44d695f197e
syifaandini <syifandin@gmail.com>
2020-02-12 13:40:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -9,39 +10,41 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
-  // function playGif(){
+// function playGif(){
   $(document).ready(function(){
     alert("hai");
-    $("#artha-project1-all, #artha-project1-img, #artha-project1-text").hover(
-        function() {
-            $("#artha-project1-img").attr("src", "images/project/gedungsate.gif");
-        },
-        function() {
-            $("#artha-project1-img").attr("src", "images/project/gunung.jpg");
-        }
+    $("#artha-project1-all").hover(
+      function() {
+        $("#artha-project1-img").attr("src", "images/project/gedungsate.gif");
+      },
+      function() {
+        $("#artha-project1-img").attr("src", "images/project/gunung.jpg");
+      }
     );
 
-    $("#artha-project2-img, #artha-project2-text").hover(
-        function() {
-            $("#artha-project2-img").attr("src", "images/project/merbabu.gif");
-        },
-        function() {
-            $("#artha-project2-img").attr("src", "images/project/langit.jpg");
-        }
+    $("#artha-project2-all").hover(
+      function() {
+        $("#artha-project2-img").attr("src", "images/project/merbabu.gif");
+      },
+      function() {
+        $("#artha-project2-img").attr("src", "images/project/langit.jpg");
+      }
     );
 
-    $("#artha-project3-img, #artha-project3-text").hover(
-        function() {
-            $("#artha-project3-img").attr("src", "images/project/merbabu2.gif");
-        },
-        function() {
-            $("#artha-project3-img").attr("src", "images/project/gunung2.jpg");
-        }
+    $("#artha-project3-all").hover(
+      function() {
+        $("#artha-project3-img").attr("src", "images/project/merbabu2.gif");
+      },
+      function() {
+        $("#artha-project3-img").attr("src", "images/project/gunung2.jpg");
+      }
     );
-  });
+    });
 
-  // }
+    // }
 
-  // window.onload = function(){
-  //   playGif();
-  // }
+    // window.onload = function(){
+      //   playGif();
+      // }
+
+// $("#artha-project1-img").
